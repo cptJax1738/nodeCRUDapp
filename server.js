50781563a2ce03957d0faf1e109f86799ea5c856
cptJax1738 <cptjax@protonmail.ch>
2017-06-05 19:29:25 -0400

    ready for database

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+
+// INIT
 console.log('May Node be with you');
 
+// DECLARING REQUIREMENTS
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 // using express is as simple as declaring an instance of the class and requiring it.
 
+app.use(bodyParser.urlencoded({extended: true})); // You must add MIDDLEWARES to EXPRESS by USING them
+
+// ** MAKE SURE TO ADD CRUD HANDLERS AFTER USING STATEMENTS
+
+// STARTING SERVER LISTENING
 app.listen(3000, function() {
 	console.log('listening on 3000')
 });
@@ -18,3 +27,8 @@ app.get('/', (request, response) => {
 // the callback function takes two arguements, request and response as objects manipulated.
 // they seem to have prototype functions built in. 
 // KEY =>> WHENEVER A CLIENT REQUESTS PAGE the ENTIRE function runs.
+
+app.post('/quotes', (req, res) => {
+	console.log('post method worked');
+	console.log(req.body);
+});
